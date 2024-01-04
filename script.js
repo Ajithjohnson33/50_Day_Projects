176e591d5952a56eee8df02355697961281354d3
Ajith Johnson <65328162+Ajithjohnson33@users.noreply.github.com>
2024-01-04 16:51:29 +0530

    latest code

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const panels = document.querySelectorAll('.panel')
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
+
+const panels = document.querySelectorAll('.pannel')
+console.log(panels[1]);
+
+panels.forEach(() => {
+     panels.addEventListener('click', () => {
         panel.classList.add('active')
-    })
+     })
 })
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
 
